d4b1e302a16c4537080af9cadf8fc05038f63ec9
Rashid <m.rashid.se@gmail.com>
2020-01-27 04:15:47 +0500

    4. BuyToken

--- contracts/FtTokenSale.sol
+++ contracts/FtTokenSale.sol
@@ -10,6 +10,12 @@ contract FtTokenSale {
 	address admin;
 	FtToken public tokenContract; 
 	uint256 public tokenPrice; 
+	uint256 public tokensSold; 
+
+	event Sell(
+	    address indexed _buyer,
+	    uint256 _amount
+	    );
 
 	constructor(FtToken _tokenContract, uint256 _tokenPrice) public {
 		// Assign an Admin
@@ -21,4 +27,27 @@ contract FtTokenSale {
 		// Token Price
 		tokenPrice = _tokenPrice;
 	}
+
+	function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "Multiply overflow!");
+    }
+
+
+	function buyTokens(uint256 _numberOfTokens) public payable {
+		// require that value is equal to token
+		require (msg.value == multiply(_numberOfTokens, tokenPrice));
+		
+		// require that contract has enough token
+		require (tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+		// require that transfer is successfull
+		require (tokenContract.transfer(msg.sender, _numberOfTokens));
+		
+		// Keep track of tokenSold
+		tokensSold += _numberOfTokens;
+		
+		// Emit Sell Event
+		emit Sell(msg.sender, _numberOfTokens);
+	}
+	
 }
--- test/FtTokenSale.js
+++ test/FtTokenSale.js
@@ -1,12 +1,19 @@
+const FtToken = artifacts.require("./FtToken.sol");
 const FtTokenSale = artifacts.require("./FtTokenSale.sol");
 
 contract("FtTokenSale",(accounts)=>{
 
+	const ADMIN = accounts[0];
+	const BUYER = accounts[1];
+	const TOKEN_PRICE = 1000000000000000; // in wei (wei is: how we keep track of ether in solidity, so wei is smallest sub-devision of ether, smaalest unit of ether,it mean we don't use float, we use wei) 
+	const TOKENS_AVAILABLE = 750000;
 
+
+	let ftTokenInstance;
 	let ftTokenSaleInstance;
-	const tokenPrice = 1000000000000000 // in wei (wei is: how we keep track of ether in solidity, so wei is smallest sub-devision of ether, smaalest unit of ether,it mean we don't use float, we use wei) 
 
 	before(async()=>{
+		ftTokenInstance = await FtToken.deployed();
 		ftTokenSaleInstance = await FtTokenSale.deployed();
 	});
 
@@ -28,7 +35,68 @@ contract("FtTokenSale",(accounts)=>{
 
 			// Has Token Price
 			const price = await ftTokenSaleInstance.tokenPrice();
-			assert.equal(price, tokenPrice,"token price is correct");			 
+			assert.equal(price, TOKEN_PRICE,"token price is correct");			 
 		});
 	});
+	
+	describe("Token Buying",async () =>{
+		var numberOfTokens = 10;
+		var value = numberOfTokens * TOKEN_PRICE;
+		
+		it("Provisions % of all Tokens to the Token Sale", async()=>{
+			const receipt = await ftTokenInstance.transfer(ftTokenSaleInstance.address, TOKENS_AVAILABLE, {from: ADMIN});
+		});
+
+		it("Emit sell event on buying", async()=>{
+			const receipt = await ftTokenSaleInstance.buyTokens(numberOfTokens, {from: BUYER, value: value});
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+			assert.equal(receipt.logs[0].args._buyer, BUYER, 'logs the account that purchases the tokens');
+			assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the numberOfTokens purchased');
+			console.log(receipt.logs[0].args._amount.toNumber());
+		});
+
+		it("Increments tokensSold", async()=>{
+			const amount = await ftTokenSaleInstance.tokensSold();
+			assert.equal(amount.toNumber(), numberOfTokens, "increments the number of tokens sold")
+
+		});
+
+		it("Buyer balance", async()=>{
+			const balance = await ftTokenInstance.balanceOf(BUYER);
+			assert.equal(balance.toNumber(), numberOfTokens, "buyer balance")
+		});
+
+		it("Reduce available token after sold", async()=>{
+			const balance = await ftTokenInstance.balanceOf(ftTokenSaleInstance.address);
+			assert.equal(balance.toNumber(), TOKENS_AVAILABLE - numberOfTokens, "increments the number of tokens sold")
+		});
+		
+		it("Stop buying with wrong value", async()=>{
+			let err = null;
+			try {
+				await ftTokenSaleInstance.buyTokens(numberOfTokens, {from: BUYER, value: 1});
+			} catch (error) {
+				err = error;
+			}
+			assert.ok(err instanceof Error,"stop buying different from ether value");
+		});
+
+		it("Stop buying more than available for Token Sale", async()=>{
+			let err = null;
+			let moreThanAvailable = (TOKENS_AVAILABLE + 100);
+			let val = (moreThanAvailable * TOKEN_PRICE);
+			try {
+				await ftTokenSaleInstance.buyTokens(moreThanAvailable, {from: BUYER, value });
+			} catch (error) {
+				err = error;
+				console.log(err.message);
+			}
+			assert.ok((err instanceof Error) && (err.message.indexOf("revert") >= 0),"Stop buying more than available for Token Sale");
+			// assert.ok(err instanceof Error,"Stop buying more than available for Token Sale");
+		});
+		
+	});
+
+
 });
